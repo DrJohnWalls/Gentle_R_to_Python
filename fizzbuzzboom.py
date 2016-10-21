d11c283e4f4317a9b82ce6a01b890f39e7a26ffa
jmausolf <jgm346@gmail.com>
2016-10-21 03:49:31 -0500

    Example of same function in Python

---
+++ fizzbuzzboom.py
@@ -0,0 +1,49 @@
+
+import time
+import argparse
+
+def fizzbuzzboom(input_value):
+    """
+    The fizzbuzzboom function takes numeric inputs and provides a result.
+    """
+
+    #Value Checking
+    try:
+        x = int(input_value)
+        pass
+    except ValueError:
+        #Handle the exception
+        print ("Please enter an integer")
+        return
+
+    if x == 524:
+        delay = 0.5
+        print("You have entered the self destruct code...")
+        time.sleep(delay)
+        print("All your files will be erased...")
+        time.sleep(delay)
+        print("You have 5 seconds to abort...")
+        t = 5+1
+        time.sleep(delay)
+        while t > 1:
+            t = t-1
+            print(t)
+            time.sleep(delay*2);
+        print("[*] BOOM")
+    elif x % 5==0 and x % 3==0:
+        print("fizzbuzz")
+    elif x % 5==0:
+        print("buzz")
+    elif x % 3==0:
+        print("fizz")
+    else:
+        print(x)
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("number", default=15, type=float, help="submit an integer or float to fizzbuzzboom")
+    args = parser.parse_args()
+
+    print("[*] Calculating the result...")
+    fizzbuzzboom(args.number)
